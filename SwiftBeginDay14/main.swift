1c82dbc7e800c70903be673c3593930e90b1d6ae
TakahashiIkki <new_ittyandesu@outlook.jp>
2018-06-12 23:16:24 +0900

    APIRequest部分を実装

--- SwiftBeginDay14/main.swift
+++ SwiftBeginDay14/main.swift
@@ -8,4 +8,40 @@
 
 import Foundation
 
+print("Enter Your Query here> ", terminator: "")
 
+guard let keyword = readLine(strippingNewline: true) else {
+    exit(1)
+}
+
+// APIクライアントの生成
+
+let clinent = GithubClient()
+
+// リクエストの発行
+let request = GithubAPI.SearchRepositories(keyeord: keyword)
+
+clinent.send(request: request) { result in
+    switch result {
+    case let .success(response):
+        for item in response.items {
+            // レポジトリの所有者と名前を出力
+            print(item.owner.login + "/" + item.name)
+        }
+        exit(0)
+    case let .failure(error):
+        print(error)
+        exit(1)
+    }
+}
+
+
+// タイムアウト時間
+let timeoutInterval: TimeInterval = 60
+
+// タイムアウトまでメインスレッドを停止
+Thread.sleep(forTimeInterval: timeoutInterval)
+
+// タイムアウト後の処理
+print("Connection timeout")
+exit(1)
