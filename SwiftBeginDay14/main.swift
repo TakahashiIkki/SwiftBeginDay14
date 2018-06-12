cde22f8612154deace5fd766dca0821ab4edd4e4
Takashi Ikki <takahashi@omicale.com>
2018-06-12 13:37:27 +0900

    Initial Commit

---
+++ SwiftBeginDay14/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftBeginDay14
+//
+//  Created by 髙橋一騎 on 2018/06/12.
+//  Copyright © 2018年 髙橋一騎. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
